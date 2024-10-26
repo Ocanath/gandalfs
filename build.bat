80fa6b4a6b37956ec24c6ef07846dcb057e96e6f
Jesse Cornman <ocanath@gmail.com>
2024-10-26 01:55:38 -0700

    what have i done...

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --onefile --windowed --add-data "GANDALFS.mp4;." --add-binary "D:\VIDEOTANGS\Youtubes\ffmpeg.exe;." play_gandalfs.py
+pyinstaller --onefile --windowed --add-data "GANDALFS.mp4;." --add-data "audio.mp3;." play_gandalfs.py
